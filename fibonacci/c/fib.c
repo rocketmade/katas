eb0881307a5003b4f98c171076f8b339f56ae03e
Brandon Roth <brandon@rocketmade.com>
2014-01-19 22:24:30 -0700

    Add c solution for fibonacci

---
+++ fibonacci/c/fib.c
@@ -0,0 +1,62 @@
+//to compile: gcc -o fib.c
+//then ./fib 10
+//
+
+#include <stdio.h> //needed for printf and sscanf
+#include <stdlib.h> //needed for malloc and free
+
+int fib(int n);
+int goodFib(int n);
+
+int main(int argc, char *argv[])
+{
+	int value = 0;
+	if (argc == 2)
+	{
+		sscanf(argv[1],"%i",&value);
+		printf("Fib %i = %i\n",value, fib(value));
+		printf("Good Fib %i = %i\n",value, goodFib(value));
+	}
+	else
+	{
+		printf("Useage: fib x\n");
+	}
+}
+
+//Fib is the naive exponential recursive solution
+int fib(int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	return fib(n-1) + fib(n-2);
+}
+
+//goodFib uses memoization to achieve a runtime of n
+int goodFib(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+
+	int *memo = (int *)malloc(n*sizeof(int));
+
+	//we can acomplish the same thing in c++ with the new operator
+	//int *memo = new int[n];
+
+	memo[0] = 0; memo[1] = 1;
+
+	for (int i = 2; i <= n; ++i)
+	{
+		memo[i] = memo[i-1] + memo[i-2];
+	}
+
+	free(memo);
+
+	//if using c++ we need to use the delete[] operator
+	//delete[] memo;
+
+	return memo[n];
+}
