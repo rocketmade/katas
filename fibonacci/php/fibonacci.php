bcfe34d17f2cbbe3c853d7c69d831dce0eda184a
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2014-02-25 19:14:41 -0700

    implemented recursive fibonacci, modeled after Daniel's ruby solution

--- fibonacci/php/fibonacci.php
+++ fibonacci/php/fibonacci.php
@@ -1,40 +1,8 @@
-#!/usr/bin/env php
 <?php
-	/*	Fibonacci Solution 	**
-	**	by Bradyn Poulsen 	**
-	**						**
-	**	January 31, 2014	**
-	**
-	**	Usage: ./fibonacci.php [--test] <n> [<n....>]
-	**		--test Test using all passed values (strings use the ASCII value of the first character)
-	*/
-
-	function get_fibonacci($n){
-		$prev = 0;
-		$curr = 1;
-		for($i = 0;$i < $n;$i++):
-			$old_prev = $prev;
-			$prev = $curr;
-			$curr = $old_prev + $prev;
-		endfor;
-		return $curr;
+	$_FIBONACCI = array(0,1);
+	function fibonacci($n){
+		global $_FIBONACCI;
+		if(!isset($_FIBONACCI[$n])) $_FIBONACCI[$n] = fibonacci($n-1) + fibonacci($n-2);
+		return $_FIBONACCI[$n];
 	}
-
-	if($argv[1] == "--test"):
-		$test_on = array(0,1,2,3,4,5,10,30,50,100);
-		if($argv[2]):
-			$test_on = $argv;
-			unset($test_on[0], $test_on[1]);
-		endif;
-
-		$start = microtime();
-		foreach($test_on as $value):
-			echo 'Fibonacci '.number_format($value).': '.number_format(get_fibonacci($value)).PHP_EOL;
-		endforeach;
-		echo 'Tested '.sizeof($test_on).' values in '.number_format(microtime() - $start,5).' seconds'.PHP_EOL;
-	elseif(is_numeric($argv[1])):
-		echo get_fibonacci($argv[1]).PHP_EOL;
-	else:
-		echo 'Nothing to do.'.PHP_EOL;
-	endif;
 ?>
