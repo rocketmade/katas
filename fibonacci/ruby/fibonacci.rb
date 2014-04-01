475a13a546b0247f607ac307e55e8fa72758b753
Daniel Evans <evans.daniel.n@gmail.com>
2014-03-31 23:46:25 -0600

    Avoid stack too deep errors calculating fibonaccis in ruby

--- fibonacci/ruby/fibonacci.rb
+++ fibonacci/ruby/fibonacci.rb
@@ -1,7 +1,10 @@
 
+
 Fibonacci = Hash.new do |h,k|
-  h[k] = h[k-1] + h[k-2]
+  (1..(k-2)).step(100).each { |i| h[i] } if k > 100 && !h.has_key?(k-100) # this is useful for avoiding stack too deep errors
+  h[k] = h[k-2] + h[k-1]
 end.merge! 0 => 0, 1 => 1
+
 
 class Fixnum
   def fibonacci
